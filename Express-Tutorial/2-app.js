ebf3308533e8cb6b402e6e20bd034a09c923a528
Rusmia-Rahaman-Urfa <156609609+Rusmia-Rahaman-Urfa@users.noreply.github.com>
2025-06-28 22:52:45 +0600

    Add files via upload

---
+++ Express-Tutorial/2-app.js
@@ -0,0 +1,13 @@
+// Globals - No WINDOW !!!
+
+//  __dirname - path to current directory
+//  __filename - file name
+//  reguire - function to use Modules(Common JS)
+//  module - info about current module (file)
+//  process - info about where the program is being executed
+
+console.log(__dirname)
+
+setInterval(() => {
+    console.log('hello world!!!')
+},1000)
---
+++ Express-Tutorial/3-app.js
@@ -0,0 +1,12 @@
+// Modules
+const jhon = 'jhon'
+const peter = 'peter'
+const Rusmia = 'Rusmia'
+
+const sayHi = (name) => {
+    console.log(`Hello my name is ${name}`)
+}
+
+sayHi(jhon)
+sayHi(peter)
+sayHi(Rusmia)
