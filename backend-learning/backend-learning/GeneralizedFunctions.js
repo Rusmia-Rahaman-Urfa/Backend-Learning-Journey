10a8860355fa66277ff2f3778268d8a60a86a960
Rusmia Rahaman Urfa <156609609+Rusmia-Rahaman-Urfa@users.noreply.github.com>
2025-07-31 02:28:47 +0600

    Add files via upload

---
+++ backend-learning/backend-learning/GeneralizedFunctions.js
@@ -0,0 +1,10 @@
+function squareNum(num)
+{
+    return num * num;
+}
+const result10 = squareNum(10);
+const result9 = squareNum(9);
+const result8 = squareNum(8);
+console.log("output for 10: ",result10);
+console.log("output for 9: ",result9);
+console.log("output for 8: ",result8);
