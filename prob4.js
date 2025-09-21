634222973ba2c99020d860808f21fc0949530580
Rusmia Rahaman Urfa <156609609+Rusmia-Rahaman-Urfa@users.noreply.github.com>
2025-09-21 17:34:17 +0600

    Add files via upload

---
+++ prob4.js
@@ -0,0 +1,8 @@
+// Create an array of 3 colors and print each color using a for loop.
+
+const color = ["Red", "Orange", "Blue"];
+let text = ""
+for(let i = 0; i < color.length; i++){
+    text += color[i];
+}
+ console.log(color);
