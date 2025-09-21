77fbe1769e81fa838a6be1adade8d10434f25169
Rusmia Rahaman Urfa <156609609+Rusmia-Rahaman-Urfa@users.noreply.github.com>
2025-09-22 02:34:18 +0600

    Add files via upload

---
+++ prob8.js
@@ -0,0 +1,13 @@
+//  Ask the user for their age using prompt() and display:
+// "You are an adult" if age ≥ 18
+// "You are a minor" otherwise
+
+
+let age;
+age = window.prompt("Tell me your age");
+
+if(age >=  18){
+    console.log("You are adult");
+}else{
+    console.log("You are minor");
+}
