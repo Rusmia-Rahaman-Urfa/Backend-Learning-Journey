600ea4820ae11fab444b0268699daf0ea8c87ab8
Rusmia Rahaman Urfa <156609609+Rusmia-Rahaman-Urfa@users.noreply.github.com>
2025-09-23 02:20:17 +0600

    Add files via upload

---
+++ prob9.js
@@ -0,0 +1,7 @@
+// Write a function that takes three numbers and returns the largest.
+
+
+function largestNum(num1 , num2, num3){
+      return Math.max(num1, num2, num3);
+}
+console.log(largestNum(4, 7, 10));
