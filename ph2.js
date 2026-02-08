7fc66f613b4776f4987bd70c1912c0143ecd415e
rusmiarahaman77461@gmail.com <rusmiarahaman77461@gmail.com>
2026-02-08 17:25:56 +0600

    Valid Otp Problem

---
+++ ph2.js
@@ -0,0 +1,20 @@
+function validOtp(otp) {
+    if(typeof otp != 'string'){
+        return "Invalid";
+    }
+    if(otp.length == 8 && otp.startsWith("ph-")){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+console.log(validOtp("ph-10985")); // Output: true 
+console.log(validOtp("ph-1234")); // Output: false
+console.log(validOtp("abc-12345"));    // Output: false 
+console.log(validOtp(["ph-10985"]));   // Output: Invalid 
+console.log(validOtp(12345678)); // Output: Invalid 
+
+
+
+
